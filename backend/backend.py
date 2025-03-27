74d463f018218209cfd7c4b36070fbb04ef4b10a
firefist420 <bearded_dad_69@outlook.com>
2025-03-26 21:04:53 -0400

    Backend requirements fixes

--- backend/backend.py
+++ backend/backend.py
@@ -18,7 +18,7 @@ from fastapi.responses import JSONResponse
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from jose import jwt, JWTError
 from contextlib import asynccontextmanager
-from pydantic_settings import BaseSettings
+from pydantic import BaseSettings
 import time
 
 logging.basicConfig(level=logging.INFO)
