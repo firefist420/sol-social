b8d55b4ac4bf79b76cbc322617305fc211e23f62
firefist420 <bearded_dad_69@outlook.com>
2025-03-26 21:17:30 -0400

    Backend database url

--- backend/backend.py
+++ backend/backend.py
@@ -20,6 +20,11 @@ from jose import jwt, JWTError
 from contextlib import asynccontextmanager
 from pydantic import BaseSettings
 import time
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
